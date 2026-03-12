cd59f8ac99203211b56f7dc85b5b28617d7ce44d
남진우 <realize2578@gmail.com>
2026-03-12 11:26:08 +0900

    [feat]: 수정 dto, 더티체킹 수정 로직 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -69,6 +70,43 @@ public class ApiV1PostController {
                 )
         );
     }
+
+
+    record PostModifyReqBody(
+            @Size(min = 2, max = 10, message = "03-title-제목은 2자 이상 10자 이하로 입력해주세요.")
+            @NotBlank(message = "01-title-제목은 필수입니다.")
+            String title,
+
+            @NotBlank(message = "02-content-내용은 필수입니다.")
+            @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상 100자 이하로 입력해주세요.")
+            String content
+    ) {
+    }
+
+    record PostModifyResBody(
+            PostDto postDto
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional //더티체킹
+    public RsData<PostModifyResBody> modify(
+            @PathVariable int id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+
+        Post post = postService.modify(id, reqBody.title, reqBody.content);
+
+        return new RsData<>(
+                "%d번 글이 성공적으로 수정되었습니다.".formatted(post.getId()),
+                "200-1",
+                new PostModifyResBody(
+                        new PostDto(post)
+                )
+        );
+    }
+
+
 
     @DeleteMapping("/{id}")
     public RsData<Void> delete(
